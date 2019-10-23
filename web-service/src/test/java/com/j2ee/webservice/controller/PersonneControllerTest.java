3bf03b5cf9465bdd09bf742c9bd75d69e4b2708b
Armando La Placa <armando.laplaca@gmail.com>
2019-10-23 18:00:03 +0200

    added test for getById and getAll

--- web-service/src/test/java/com/j2ee/webservice/controller/PersonneControllerTest.java
+++ web-service/src/test/java/com/j2ee/webservice/controller/PersonneControllerTest.java
@@ -3,6 +3,7 @@ package com.j2ee.webservice.controller;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.junit.Test;
@@ -10,6 +11,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.j2ee.webservice.model.Personne;
@@ -17,19 +19,24 @@ import com.j2ee.webservice.model.Personne;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PersonneControllerTest {
-	
+
+	@LocalServerPort
+	private int port = 8080;
+
 	@Autowired
 	private TestRestTemplate restTemplate;
 
 	@Test
 	public void testGetAll() {
-		List<Personne> lista = this.restTemplate.getForObject("http://localhost:8080/personnes", List.class);
+		List<Personne> lista = this.restTemplate.getForObject("http://localhost:" + port+"/personnes", List.class);
 		assertEquals(5, lista.size());
 	}
 
 	@Test
 	public void testGetById() {
-		//list=pc.getAll();
+		List<HashMap> lista = this.restTemplate.getForObject("http://localhost:" + port+"/personnes", List.class);
+		Personne output = this.restTemplate.getForObject("http://localhost:" + port+"/personnes/2", Personne.class);
+		assertEquals(lista.get(1).get("nom"), output.getNom());
 	}
 
 	@Test
