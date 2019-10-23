7ad3c48ee0b1a1f19507ffb3c4892afa82e16c95
Armando La Placa <armando.laplaca@gmail.com>
2019-10-23 17:27:08 +0200

    fixed again

--- web-service/src/test/java/com/j2ee/webservice/controller/PersonneControllerTest.java
+++ web-service/src/test/java/com/j2ee/webservice/controller/PersonneControllerTest.java
@@ -3,22 +3,35 @@ package com.j2ee.webservice.controller;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
-import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Collection;
+import java.util.List;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.j2ee.webservice.model.Personne;
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.Collections;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PersonneControllerTest {
 	
 	@Autowired
-	private PersonneController pc = new PersonneController();
+	private TestRestTemplate restTemplate;
 
 	@Test
 	public void testGetAll() {
-		assertEquals(0, pc.getAll().size());
+		List<Personne> lista = (List<Personne>) this.restTemplate.getForObject("localhost:8080/personnes", Collection.class);
+		assertEquals(5, lista.size());
 	}
 
 	@Test
 	public void testGetById() {
-		fail("Not yet implemented");
+		//list=pc.getAll();
 	}
 
 	@Test
